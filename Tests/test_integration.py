78d1d2cea83996d84ce76eefd37ccefb63ad4e91
Nilesh Saumyadasa <110282720+manikkuge-saumyadasa@users.noreply.github.com>
2026-04-30 12:53:00 -0500

    Invalid Integration test

--- Tests/test_integration.py
+++ Tests/test_integration.py
@@ -3,4 +3,4 @@ from Calculator_Code.calc import add, subtract, multiply, divide
 def test_combined_operations():
     result = divide(multiply(add(2, 3), 4), 2)
     # ((2+3) * 4) / 2 = 10
-    assert result == 10
+    assert result == 5
