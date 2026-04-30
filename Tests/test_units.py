dd645dba83e4b9a7ddf0f57d4438856aa81663d6
Nilesh Saumyadasa <110282720+manikkuge-saumyadasa@users.noreply.github.com>
2026-04-30 12:40:33 -0500

    Add CI pipeline, calculator implementation, and unit/integration tests

---
+++ Tests/test_units.py
@@ -0,0 +1,13 @@
+from Calculator_Code.calc import add, subtract, multiply, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(2, 4) == 8
+
+def test_divide():
+    assert divide(10, 2) == 5
